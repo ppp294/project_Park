fcda2fb3e21f68636b49f25c367be00a6a6f9348
ppp294 <ppp294@naver.com>
2021-04-26 17:34:27 +0900

    자바1일차:주석 설명 예제

---
+++ Java_Park/src/day1/HellowWorld.java
@@ -0,0 +1,20 @@
+package day1;
+
+/**
+ * 도큐먼트 주석
+ * 
+ * @author green
+ *
+ */
+public class HellowWorld {
+//주석
+	/* 작성일 : 2021-04-26
+	 * 작성자 : 박성민
+	 * 내용  : 기본 프로젝트 생성 및 테스트
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello wolrd!");
+
+	}
+
+}
